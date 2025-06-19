939b7d78bd51150fe9629d3a6cc9c5dc7b2c5f32
Cyanmin <tomohiro20030220@gmail.com>
2025-06-20 01:21:34 +0900

    Revert "Integrate react-query"

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { QueryClientProvider } from '@tanstack/react-query'
 import App from './App'
-import { queryClient } from './queryClient'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <App />
   </React.StrictMode>
 )
